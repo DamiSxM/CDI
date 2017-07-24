10546e505320207729375df66d54c38751b7d34c
Dami <dami.bachelet@gmail.com>
2017-07-24 14:05:23 +0200

    après midi

---
+++ MOD2/SEQ2/SEA3/A2b/A2 2.6/A2/Form2.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace A2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label productIDLabel;
+            System.Windows.Forms.Label productNameLabel;
+            System.Windows.Forms.Label unitPriceLabel;
+            System.Windows.Forms.Label categoryIDLabel;
+            this.comptoirSimplifieDataSet = new A2.ComptoirSimplifieDataSet();
+            this.productsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.productsTableAdapter = new A2.ComptoirSimplifieDataSetTableAdapters.ProductsTableAdapter();
+            this.tableAdapterManager = new A2.ComptoirSimplifieDataSetTableAdapters.TableAdapterManager();
+            this.productsComboBox = new System.Windows.Forms.ComboBox();
+            this.productIDTextBox = new System.Windows.Forms.TextBox();
+            this.productNameTextBox = new System.Windows.Forms.TextBox();
+            this.unitPriceTextBox = new System.Windows.Forms.TextBox();
+            this.categoryIDTextBox = new System.Windows.Forms.TextBox();
+            this.categoriesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.categoriesTableAdapter = new A2.ComptoirSimplifieDataSetTableAdapters.CategoriesTableAdapter();
+            this.categoriesComboBox = new System.Windows.Forms.ComboBox();
+            productIDLabel = new System.Windows.Forms.Label();
+            productNameLabel = new System.Windows.Forms.Label();
+            unitPriceLabel = new System.Windows.Forms.Label();
+            categoryIDLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.comptoirSimplifieDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.categoriesBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comptoirSimplifieDataSet
+            // 
+            this.comptoirSimplifieDataSet.DataSetName = "ComptoirSimplifieDataSet";
+            this.comptoirSimplifieDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productsBindingSource
+            // 
+            this.productsBindingSource.DataMember = "Products";
+            this.productsBindingSource.DataSource = this.comptoirSimplifieDataSet;
+            // 
+            // productsTableAdapter
+            // 
+            this.productsTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.CategoriesTableAdapter = this.categoriesTableAdapter;
+            this.tableAdapterManager.CustomersTableAdapter = null;
+            this.tableAdapterManager.Order_DetailsTableAdapter = null;
+            this.tableAdapterManager.OrdersTableAdapter = null;
+            this.tableAdapterManager.PaysTableAdapter = null;
+            this.tableAdapterManager.ProductsTableAdapter = this.productsTableAdapter;
+            this.tableAdapterManager.UpdateOrder = A2.ComptoirSimplifieDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // productsComboBox
+            // 
+            this.productsComboBox.DataSource = this.productsBindingSource;
+            this.productsComboBox.DisplayMember = "ProductName";
+            this.productsComboBox.FormattingEnabled = true;
+            this.productsComboBox.Location = new System.Drawing.Point(12, 12);
+            this.productsComboBox.Name = "productsComboBox";
+            this.productsComboBox.Size = new System.Drawing.Size(300, 21);
+            this.productsComboBox.TabIndex = 1;
+            this.productsComboBox.ValueMember = "ProductID";
+            // 
+            // productIDLabel
+            // 
+            productIDLabel.AutoSize = true;
+            productIDLabel.Location = new System.Drawing.Point(80, 79);
+            productIDLabel.Name = "productIDLabel";
+            productIDLabel.Size = new System.Drawing.Size(61, 13);
+            productIDLabel.TabIndex = 1;
+            productIDLabel.Text = "Product ID:";
+            // 
+            // productIDTextBox
+            // 
+            this.productIDTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.productsBindingSource, "ProductID", true));
+            this.productIDTextBox.Location = new System.Drawing.Point(164, 76);
+            this.productIDTextBox.Name = "productIDTextBox";
+            this.productIDTextBox.Size = new System.Drawing.Size(100, 20);
+            this.productIDTextBox.TabIndex = 2;
+            // 
+            // productNameLabel
+            // 
+            productNameLabel.AutoSize = true;
+            productNameLabel.Location = new System.Drawing.Point(80, 105);
+            productNameLabel.Name = "productNameLabel";
+            productNameLabel.Size = new System.Drawing.Size(78, 13);
+            productNameLabel.TabIndex = 3;
+            productNameLabel.Text = "Product Name:";
+            // 
+            // productNameTextBox
+            // 
+            this.productNameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.productsBindingSource, "ProductName", true));
+            this.productNameTextBox.Location = new System.Drawing.Point(164, 102);
+            this.productNameTextBox.Name = "productNameTextBox";
+            this.productNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.productNameTextBox.TabIndex = 4;
+            // 
+            // unitPriceLabel
+            // 
+            unitPriceLabel.AutoSize = true;
+            unitPriceLabel.Location = new System.Drawing.Point(80, 131);
+            unitPriceLabel.Name = "unitPriceLabel";
+            unitPriceLabel.Size = new System.Drawing.Size(56, 13);
+            unitPriceLabel.TabIndex = 5;
+            unitPriceLabel.Text = "Unit Price:";
+            // 
+            // unitPriceTextBox
+            // 
+            this.unitPriceTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.productsBindingSource, "UnitPrice", true));
+            this.unitPriceTextBox.Location = new System.Drawing.Point(164, 128);
+            this.unitPriceTextBox.Name = "unitPriceTextBox";
+            this.unitPriceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.unitPriceTextBox.TabIndex = 6;
+            // 
+            // categoryIDLabel
+            // 
+            categoryIDLabel.AutoSize = true;
+            categoryIDLabel.Location = new System.Drawing.Point(80, 157);
+            categoryIDLabel.Name = "categoryIDLabel";
+            categoryIDLabel.Size = new System.Drawing.Size(66, 13);
+            categoryIDLabel.TabIndex = 7;
+            categoryIDLabel.Text = "Category ID:";
+            // 
+            // categoryIDTextBox
+            // 
+            this.categoryIDTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.productsBindingSource, "CategoryID", true));
+            this.categoryIDTextBox.Location = new System.Drawing.Point(164, 154);
+            this.categoryIDTextBox.Name = "categoryIDTextBox";
+            this.categoryIDTextBox.Size = new System.Drawing.Size(100, 20);
+            this.categoryIDTextBox.TabIndex = 8;
+            // 
+            // categoriesBindingSource
+            // 
+            this.categoriesBindingSource.DataMember = "Categories";
+            this.categoriesBindingSource.DataSource = this.comptoirSimplifieDataSet;
+            // 
+            // categoriesTableAdapter
+            // 
+            this.categoriesTableAdapter.ClearBeforeFill = true;
+            // 
+            // categoriesComboBox
+            // 
+            this.categoriesComboBox.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.productsBindingSource, "CategoryID", true));
+            this.categoriesComboBox.DataSource = this.categoriesBindingSource;
+            this.categoriesComboBox.DisplayMember = "CategoryName";
+            this.categoriesComboBox.FormattingEnabled = true;
+            this.categoriesComboBox.Location = new System.Drawing.Point(83, 199);
+            this.categoriesComboBox.Name = "categoriesComboBox";
+            this.categoriesComboBox.Size = new System.Drawing.Size(300, 21);
+            this.categoriesComboBox.TabIndex = 9;
+            this.categoriesComboBox.ValueMember = "CategoryID";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 261);
+            this.Controls.Add(this.categoriesComboBox);
+            this.Controls.Add(productIDLabel);
+            this.Controls.Add(this.productIDTextBox);
+            this.Controls.Add(productNameLabel);
+            this.Controls.Add(this.productNameTextBox);
+            this.Controls.Add(unitPriceLabel);
+            this.Controls.Add(this.unitPriceTextBox);
+            this.Controls.Add(categoryIDLabel);
+            this.Controls.Add(this.categoryIDTextBox);
+            this.Controls.Add(this.productsComboBox);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.comptoirSimplifieDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.categoriesBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ComptoirSimplifieDataSet comptoirSimplifieDataSet;
+        private System.Windows.Forms.BindingSource productsBindingSource;
+        private ComptoirSimplifieDataSetTableAdapters.ProductsTableAdapter productsTableAdapter;
+        private ComptoirSimplifieDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.ComboBox productsComboBox;
+        private System.Windows.Forms.TextBox productIDTextBox;
+        private System.Windows.Forms.TextBox productNameTextBox;
+        private System.Windows.Forms.TextBox unitPriceTextBox;
+        private System.Windows.Forms.TextBox categoryIDTextBox;
+        private ComptoirSimplifieDataSetTableAdapters.CategoriesTableAdapter categoriesTableAdapter;
+        private System.Windows.Forms.BindingSource categoriesBindingSource;
+        private System.Windows.Forms.ComboBox categoriesComboBox;
+    }
+}
---
+++ MOD2/SEQ2/SEA3/A2b/A2 2.6/A2/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void productsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.productsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.comptoirSimplifieDataSet);
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: cette ligne de code charge les données dans la table 'comptoirSimplifieDataSet.Categories'. Vous pouvez la déplacer ou la supprimer selon les besoins.
+            this.categoriesTableAdapter.Fill(this.comptoirSimplifieDataSet.Categories);
+            // TODO: cette ligne de code charge les données dans la table 'comptoirSimplifieDataSet.Products'. Vous pouvez la déplacer ou la supprimer selon les besoins.
+            this.productsTableAdapter.Fill(this.comptoirSimplifieDataSet.Products);
+
+        }
+    }
+}
